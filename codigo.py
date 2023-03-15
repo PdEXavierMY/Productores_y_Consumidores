ae21aa456a5affea67e266f5bb203d62b763221e
Xavitheforce <javier.miguelez.yague@gmail.com>
2023-03-15 16:38:11 +0100

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,3 +1,9 @@
 from threading import Thread # Importamos la clase Thread
 import time # Importamos la libreria time
 from queue import Queue # Importamos la clase Queue
+
+
+buffer = Queue(30)
+tam_buff = 30
+buff = [0 for i in range(tam_buff + 1)]
+
