7f9580dac39abf73d40094f50af967cbba5d5abe
Xavitheforce <javier.miguelez.yague@gmail.com>
2023-03-15 16:52:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-import time
-from threading import Thread
-from codigo import buffer, tam_buff, buff, productor, consumidor
+import time # Importamos la libreria time
+from threading import Thread # Importamos la clase Thread
+from codigo import buffer, tamano_buff, buff, productor, consumidor # Importamos las variables y funciones del archivo codigo.py
 
-def main():
-    total_productores = 3
-    total_consumidores = 2
-    productores = []
-    consumidores = []
-    start_time = time.time()
-    for i in range(total_productores):
-        productores.append(Thread(target=productor, args=(i,)))
-        productores[i].start()
-    for i in range(total_consumidores):
-        consumidores.append(Thread(target=consumidor, args=(i,)))
-        consumidores[i].start()
-    for i in range(total_productores):
-        productores[i].join()
-    for i in range(total_consumidores):
-        consumidores[i].join()
+def main(): # Funcion principal
+    total_productores = 2 # Variable que contiene el numero de productores
+    total_consumidores = 2 # Variable que contiene el numero de consumidores
+    productores = [] # Creamos un arreglo de productores
+    consumidores = [] # Creamos un arreglo de consumidores
+    start_time = time.time() # Variable que contiene el tiempo de inicio
+    for i in range(total_productores): # Creamos los productores
+        productores.append(Thread(target=productor, args=(i,))) # Creamos un objeto de la clase Thread y lo inicializamos con la funcion productor y el id del productor por cada productor definido
+        productores[i].start() # Iniciamos el hilo
+    for i in range(total_consumidores): # Creamos los consumidores de manera similar a los productores
+        consumidores.append(Thread(target=consumidor, args=(i,))) # Creamos un objeto de la clase Thread y lo inicializamos con la funcion consumidor y el id del consumidor por cada consumidor definido
+        consumidores[i].start() # Iniciamos el hilo
+    for i in range(total_productores): # Esperamos a que los productores terminen
+        productores[i].join() # Esperamos a que el hilo termine
+    for i in range(total_consumidores): # Esperamos a que los consumidores terminen
+        consumidores[i].join() # Esperamos a que el hilo termine
     print("Buffer: ", buffer.qsize())
-    print("Total productores: ", total_productores)
-    print("Total consumidores: ", total_consumidores)
-    print("Ocupacion del Buffer: ")
-    for i in range(tam_buff + 1):
+    print("Total de productores: ", total_productores)
+    print("Total de consumidores: ", total_consumidores)
+    print("Ocupación del Buffer: ")
+    for i in range(tamano_buff + 1): # Imprimimos el numero de elementos en cada celda del buffer
         print('Celda [', i, ']:', buff[i])
     print("Tiempo transcurrido: " + str(time.time() - start_time) + " segundos")
     return
